12dc710a0b9b1b744413928e7256a38cd01c5f30
김현지 <khj72463342@gmail.com>
2024-05-14 15:53:23 +0900

    [Feat] #1 - login vue 구현

---
+++ src/views/TheMainView.vue
@@ -0,0 +1,13 @@
+<script setup>
+</script>
+
+<template>
+    <div>
+        <h1>메인</h1>
+        <router-link to="/user">유저 페이지로 이동</router-link>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
