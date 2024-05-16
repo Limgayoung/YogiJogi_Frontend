0bea2394ff9ce3e579f6377f8f00c8648e2b36b8
ygy920 <ygy920@gmail.com>
2024-05-16 16:51:51 +0900

    [Feat]#9 - 왼쪽 네브바 구현 중

--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -1,12 +1,14 @@
 <script setup>
 import DefaultMap from '@/components/map/DefaultMap.vue'
 import TripMap from '@/components/map/TripMap.vue'
+import LeftNav from '@/components/map/LeftNav.vue'
 
 </script>
 
 <template>
 
     <!-- <DefaultMap></DefaultMap> -->
+    <!-- <LeftNav></LeftNav> -->
     <TripMap></TripMap>
 </template>
 
